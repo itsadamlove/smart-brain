7d65512da3b3e8a7fdcaf563d09df9f127210aa7
Adam Love <adam.love127@gmail.com>
2018-09-06 10:23:49 +1000

    Refactor code with linting tools

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -7,25 +7,25 @@ class Register extends React.Component {
       email: '',
       password: '',
       name: ''
-    }
+    };
   }
 
-  onNameChange = (event) => {
-    this.setState({name: event.target.value})
-  }
+  onNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
 
-  onEmailChange = (event) => {
-    this.setState({email: event.target.value})
-  }
+  onEmailChange = event => {
+    this.setState({ email: event.target.value });
+  };
 
-  onPasswordChange = (event) => {
-    this.setState({password: event.target.value})
-  }
+  onPasswordChange = event => {
+    this.setState({ password: event.target.value });
+  };
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch(`${process.env.REACT_APP_API_URL}/register`, {
       method: 'post',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         email: this.state.email,
         password: this.state.password,
@@ -35,11 +35,11 @@ class Register extends React.Component {
       .then(response => response.json())
       .then(user => {
         if (user.id) {
-          this.props.loadUser(user)
+          this.props.loadUser(user);
           this.props.onRouteChange('home');
         }
-      })
-  }
+      });
+  };
 
   render() {
     return (
@@ -49,7 +49,9 @@ class Register extends React.Component {
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                <label className="db fw6 lh-copy f6" htmlFor="name">
+                  Name
+                </label>
                 <input
                   className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="text"
@@ -59,7 +61,9 @@ class Register extends React.Component {
                 />
               </div>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
                 <input
                   className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="email"
@@ -69,7 +73,9 @@ class Register extends React.Component {
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
                 <input
                   className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="password"
